fc4d30ce388f65c013cb900541b4e40fa23de47b
sonu720 <sayed.jameelataj@gmail.com>
2022-04-13 14:49:05 +0000

    added script to generate the inputFile

---
+++ gencsv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#this script generates 10 random numbers.
+#Date: apr 2022
+RANDOM=$$
+for i in `seq 10`
+do
+        index=$(echo $((i-1)))
+        echo "$index, $RANDOM" >> /csvserver/inputdata 
+
+done
