c745d795749eb03af3ac7a43f64a00e49356dc89
sonu720 <sayed.jameelataj@gmail.com>
2022-04-13 14:51:32 +0000

    added 100000 entries for inputFile

--- gencsv.sh
+++ gencsv.sh
@@ -2,7 +2,7 @@
 #this script generates 10 random numbers.
 #Date: apr 2022
 RANDOM=$$
-for i in `seq 10`
+for i in `seq 100000`
 do
         index=$(echo $((i-1)))
         echo "$index, $RANDOM" >> /csvserver/inputdata 
